1f27a51bc87d6ccfdef93b411eafc182df3a0753
Florian Zeidler <zeidler.f@googlemail.com>
2012-06-28 14:54:24 +0200

    Changed jar for REST.

--- server/appEngine/appengine/src/greenxplozion/iochallenge/cbase/rest/CategoriesRestService.java
+++ server/appEngine/appengine/src/greenxplozion/iochallenge/cbase/rest/CategoriesRestService.java
@@ -4,12 +4,12 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-@Path("/challenges/{category}")
-public class ChallengesRestService {
+@Path("/categories")
+public class CategoriesRestService {
 
 	@GET
 	@Produces("text/plain")
-	public String getChallengesByCategory(){
-		return null;
+	public String getCategories(){
+		
 	}
 }

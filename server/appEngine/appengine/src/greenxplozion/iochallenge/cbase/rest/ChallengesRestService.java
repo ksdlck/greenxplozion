c43017e4f352a9405677780893091c50d18e3bc8
Florian Zeidler <zeidler.f@googlemail.com>
2012-06-28 14:22:34 +0200

    Changed configuration.

---
+++ server/appEngine/appengine/src/greenxplozion/iochallenge/cbase/rest/ChallengesRestService.java
@@ -0,0 +1,5 @@
+package greenxplozion.iochallenge.cbase.rest;
+
+public class ChallengesRestService {
+
+}
---
+++ server/appEngine/appengine/src/greenxplozion/iochallenge/cbase/rest/resources/Challenge.java
@@ -0,0 +1,5 @@
+package greenxplozion.iochallenge.cbase.rest.resources;
+
+public class Challenge {
+
+}
